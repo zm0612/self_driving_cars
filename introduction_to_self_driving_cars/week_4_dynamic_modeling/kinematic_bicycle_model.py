ed263ef56f4bacf94ce966d281b20636698549ae
Zhang Zhimeng <849742405@qq.com>
2022-02-24 23:32:25 +0800

    1.Add some files of week 4

---
+++ introduction_to_self_driving_cars/week_4_dynamic_modeling/kinematic_bicycle_model.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Bicycle:
+    def __init__(self):
+        self.xc = 0
+        self.yc = 0
+        self.theta = 0
+        self.delta = 0
+        self.beta = 0
+
+        self.L = 2
+        self.lr = 1.2
+        self.w_max = 1.22
+        self.sample_time = 0.01
+
+    def reset(self):
+        self.xc = 0
+        self.yc = 0
+        self.theta = 0
+        self.delta = 0
+        self.beta = 0
